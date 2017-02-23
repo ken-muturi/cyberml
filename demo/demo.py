63dc8920a7618bbb11c1243e3501079cd11a4ecd
Josh Bowen <mr.josh.bowen@gmail.com>
2017-02-23 12:33:49 -0700

    Demo connection working

--- demo/demo.py
+++ demo/demo.py
@@ -1,17 +1,40 @@
 import boto3
+import json
 
 client = boto3.client('machinelearning')
 
-#define modelid
-#record=name:value
-#define endpoint
+#Sample Dataset
+modelid = 'ml-WyHzVJ9rBx4'
+endpoint = 'https://realtime.machinelearning.us-east-1.amazonaws.com'
+
+print('Requesting...')
 
 response = client.predict(
-    MLModelId='string',
+    MLModelId=modelid,
     Record={
-        'string': 'string'
+        'age':'21',
+        'job':'waiter',
+        'marital':'single',
+        'education':'college',
+        'default':'none',
+        'housing':'rent',
+        'loan':'none',
+        'contact':'phone',
+        'month':'march',
+        'day_of_week':'monday',
+        'duration':'1',
+        'campaign':'1',
+        'pdays':'1',
+        'previous':'1',
+        'poutcome':'none',
+        'emp_var_rate':'1',
+        'cons_price_idx':'1',
+        'cons_conf_idx':'1',
+        'euribor3m':'1',
+        'nr_employed':'1',
+        'y':'1'
     },
-    PredictEndpoint='string'
+    PredictEndpoint=endpoint
 )
 
-#handle response
+print(json.dumps(response))
